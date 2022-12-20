1c6d59c8204b7b3d5eab6441a2b25682e3cf155a
MSiesto <miguelsiesto@gmail.com>
2022-12-20 18:09:10 +0100

    Create and Implement Styled Components

--- src/QuoteCard.js
+++ src/QuoteCard.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Box, Button, CircularProgress, Card, Typography } from "@mui/material";
+import {
+  Button,
+  CircularProgress,
+  Card,
+  Typography,
+  Container,
+  LinearProgress,
+} from "@mui/material";
 //import { makeStyles } from "@mui/styles";
 import { styled } from "@mui/material";
 import { useAsyncFn } from "react-use";
@@ -12,68 +19,92 @@ function QuoteAuthorCard() {
   }, ["https://api.quotable.io/random"]);
 
   const fetchButton = (
-    <Button onClick={() => doFetch()} variant="contained" align="center">
+    <Button
+      size="large"
+      onClick={() => doFetch()}
+      variant="contained"
+      border-radius="10px"
+      margin="50px"
+      position="fixed"
+    >
       New Quote
     </Button>
   );
-  const CustomBox = styled(Box)(({ theme }) => ({
-    raised: true,
-    width: 600,
-    margin: 500,
-    padding: 50,
-  }));
+
   const CustomCard = styled(Card)(({ theme }) => ({
-    margin: 20,
-    backgroundColor: "white",
+    display: "flex",
+    flexDirection: "column",
+    width: "auto",
+    minHeight: 300,
+    minWidth: 600,
+    margin: 500,
+    marginRight: 300,
+    padding: 60,
+    boxShadow: "0px 0px 8px ",
+  }));
+  const ButtonContainer = styled(Container)(({ theme }) => ({
+    marginBottom: 30,
+    marginTop: 80,
+    width: 300,
+    padding: 10,
+    display: "flex",
+    flexDirection: "column",
+  }));
+  const TextContainer = styled(Container)(({ theme }) => ({
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "column",
+    minHeight: 150,
+    padding: 10,
+    marginBottom: 0,
   }));
 
-  if (state.value === undefined)
-    return (
-      <CustomBox>
-        <CustomCard raised="true">
-          <div>
-            <Typography variant="h2" align="center" color="textPrimary">
-              Press the button to start
-            </Typography>
-            <Typography variant="h5" align="center" color="textSecondary">
-              ...
-            </Typography>
-          </div>
-          {fetchButton}
-        </CustomCard>
-      </CustomBox>
-    );
-  if (state.loading)
-    return (
-      <CustomBox>
-        <CustomCard raised="true">
+  function GetContent() {
+    if (state.value === undefined)
+      return (
+        <TextContainer>
+          <Typography variant="h2" align="center" color="textPrimary">
+            Press the button to start
+          </Typography>
+          <Typography variant="h5" align="center" color="textSecondary">
+            ...
+          </Typography>
+        </TextContainer>
+      );
+    if (state.loading)
+      return (
+        <TextContainer>
           <CircularProgress />
-          {fetchButton}
-        </CustomCard>
-      </CustomBox>
-    );
-  if (state.error)
+        </TextContainer>
+      );
+    if (state.error)
+      return (
+        <TextContainer>
+          <Typography>Error: {state.error.message}</Typography>
+        </TextContainer>
+      );
     return (
-      <CustomBox>
-        <CustomCard raised="true">
-          <div>Error: {state.error.message}</div>
-        </CustomCard>
-      </CustomBox>
+      <TextContainer>
+        <Typography variant="h4" align="center" color="textPrimary">
+          {state.value.content}
+        </Typography>
+        <Typography
+          marginTop={"40px"}
+          variant="h6"
+          align="center"
+          color="textSecondary"
+        >
+          {state.value.author}
+        </Typography>
+      </TextContainer>
     );
+  }
+
   return (
-    <CustomBox>
-      <CustomCard raised="true">
-        <div>
-          <Typography variant="h4" align="center" color="textPrimary">
-            {state.value.content}
-          </Typography>
-          <Typography variant="h6" align="center" color="textSecondary">
-            {state.value.author}
-          </Typography>
-        </div>
-        {fetchButton}
-      </CustomCard>
-    </CustomBox>
+    <CustomCard>
+      <GetContent />
+      <ButtonContainer theme={"buttonContainer"}>{fetchButton}</ButtonContainer>
+    </CustomCard>
   );
 }
 export default QuoteAuthorCard;
